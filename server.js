7bbf5c3b2b16f9093d2cd15d586bceed26381fd7
Sara Alkhamri <s.alkhamri@gmail.com>
2019-10-01 13:28:58 -0700

    get rewuest to /api/posts/ successful

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+
 server.get('/', (req,res) => {
     res.status(200).json({api: "api running"})
+})
+
+//api/posts
+server.get('/api/posts', (req, res) => {
+    db.find()
+    .then((db) => {
+        res.status(200).json(db)
+    })
+    .catch((err) => {
+        res.status(500).json({error: "The posts information could not be retrieved."})
+    })
 })
 
 
