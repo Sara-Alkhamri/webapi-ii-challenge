1a1207f9d88c0e6cf6e03f8c8ca1cf7e5fc2c6db
Sara Alkhamri <s.alkhamri@gmail.com>
2019-10-01 13:43:34 -0700

    Post request to /api/posts/ successful

--- server.js
+++ server.js
@@ -9,19 +9,33 @@ server.get('/', (req,res) => {
     res.status(200).json({api: "api running"})
 })
 
-//api/posts
+// get api/posts/
 server.get('/api/posts', (req, res) => {
     db.find()
     .then((db) => {
         res.status(200).json(db)
     })
-    .catch((err) => {
+    .catch((error) => {
         res.status(500).json({error: "The posts information could not be retrieved."})
     })
 })
 
-
-
+//post api/posts/
+server.post('/api/posts', (req, res) => {
+    const addPost = req.body;
+    const {tilte, contents} = req.body;
+    if (tilte, contents) {
+        db.insert(addPost)
+        .then(db => {
+            res.status(201).json(db)
+        })
+        .catch(error => {
+            res.status(500).json({error: "There was an error while saving the post to the database."})
+        })
+    } else {
+        res.status(400).json({errorMessage: "Please provide text for the comment." })
+    }
+})
 
 
 
